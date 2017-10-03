46b74c3fde9c926288a3d181a35288d7f35e0e0a
Rishi Barnwal <barnwal.rishi@gmail.com>
2017-10-03 00:42:41 -0600

    Title Screen
    Added Main Title Screen
    Followed first few steps of online tutorial
    Will await further writing to implement more
    Christ it's late
    Don't tell my mom

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : ScriptableObject {
+    [TextArea]
+    public string description;
+    public string roomName;
+
+}
